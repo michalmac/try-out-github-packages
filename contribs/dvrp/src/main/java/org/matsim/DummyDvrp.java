965295e672f6e52066e237fbdec92c8d528ed065
Michal Maciejewski <michal.mac@gmail.com>
2021-04-08 12:57:50 +0200

    mimic matsim mvn project layout

---
+++ contribs/dvrp/src/main/java/org/matsim/DummyDvrp.java
@@ -0,0 +1,7 @@
+package org.matsim;
+
+public class DummyDvrp {
+	public static void main(String[] args) {
+		System.out.println("Hello, dummy dvrp!");
+	}
+}
---
+++ matsim/src/main/java/org/matsim/DummyMatsim.java
@@ -0,0 +1,7 @@
+package org.matsim;
+
+public class DummyMatsim {
+	public static void main(String[] args) {
+		System.out.println("Hello, dummy matsim!");
+	}
+}
